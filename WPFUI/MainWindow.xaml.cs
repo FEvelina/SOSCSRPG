6970238e6e513b98d5a35ab5023dbf96d50c5da3
FEvelina <evelinaflavia13@gmail.com>
2025-03-17 21:10:11 +0200

    Added Location type, still need to figure image path

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -1,14 +1,5 @@
-﻿using System.Text;
+﻿using Engine.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Engine.ViewModels;
 
 namespace WPFUI
 {
@@ -25,6 +16,12 @@ namespace WPFUI
             _gameSession = new GameSession();
 
             DataContext = _gameSession;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;
+            _gameSession.CurrentPlayer.Level = _gameSession.CurrentPlayer.Level + 1;
         }
     }
 }
