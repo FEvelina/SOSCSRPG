4bf37fb43705edc91754c52cb780ae70718834a1
FEvelina <evelinaflavia13@gmail.com>
2025-04-08 20:27:46 +0300

    GameItem, Weapon and ItemFactory classes

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    public static class ItemFactory
+    {
+        private static List<GameItem> _standardGameItems;
+
+        //the first time anyone uses anything in this class run this function
+        static ItemFactory() 
+        {
+         _standardGameItems = new List<GameItem>();
+
+            _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
+            _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
+        }
+
+        public static GameItem CreateGameItem(int itemTypeID)
+        {
+            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeId == itemTypeID);
+
+            //cloning
+            if (standardItem != null) 
+            {
+                return standardItem.Clone();
+            }
+            return null;
+        }
+    }
+}
