5c422c4aebf25a76261ee49b36270f11a42e2fe4
FEvelina <evelinaflavia13@gmail.com>
2025-04-09 21:48:51 +0300

    -i forgot

--- Engine/Models/Location.cs
+++ Engine/Models/Location.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine.Factories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,5 +16,45 @@ namespace Engine.Models
         public string? ImageName { get; set; }
         
         public List<Quest> QuestAvailableHere { get; set; } = new List<Quest>();
+        public List<MonsterEncounter> MonsterEncounterHere { get; set;} = new List<MonsterEncounter>();
+
+        public void AddMonster(int monsterID, int chanceOfEncountering)
+        {
+            if (MonsterEncounterHere.Exists( m => m.MonsterID == monsterID))
+            {
+                //if a monster already exists at the location, overwrite the encounter
+                MonsterEncounterHere.First(m => m.MonsterID == monsterID).ChanceOfEncountering = chanceOfEncountering;
+            }
+            else
+            {
+                //no monster there, add a new one
+                MonsterEncounterHere.Add(new MonsterEncounter(monsterID, chanceOfEncountering));
+            }
+        }
+
+        public Monster GetMonster()
+        {
+            if(!MonsterEncounterHere.Any())
+            { return null; }
+            //total encounter percentage of all monsters at this location
+            int totalChances = MonsterEncounterHere.Sum(m => m.ChanceOfEncountering);
+
+            int randomNumber = RandomNrGenerator.NumberBetween(1, totalChances);
+
+            int runningTotal = 0;
+
+            //loop through the list of monsters that can be encountered here, and check which's chances fit the randomNumber check, 
+            // and we return the instance of the monster matching that id
+            foreach(MonsterEncounter enemy in MonsterEncounterHere)
+            {
+                runningTotal += enemy.ChanceOfEncountering;
+                if(randomNumber <= runningTotal)
+                {
+                    return MonsterFactory.GetMonster(MonsterEncounterHere.Last().MonsterID);
+                }
+            }
+            //if problem, return last monster in the list
+            return MonsterFactory.GetMonster(MonsterEncounterHere.Last().MonsterID);
+        }
     }
 }
